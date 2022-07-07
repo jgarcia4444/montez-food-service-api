bf135df0fbbc61fb4e4b99919e289e433b9c937a
Jake Garcia <bballwiz4@gmail.com>
2022-07-07 14:25:21 -0700

    Created a function that generates a random six digit code and saves it to the instance of the user.

--- app/models/User.rb
+++ app/models/User.rb
@@ -4,4 +4,14 @@ class User < ApplicationRecord
     validates :company_name, presence: true
     validates :email, uniqueness: true
     validates :company_name, uniqueness: true
+
+    def create_ota_code
+        ota_string = ""
+        6.times do |i|
+            random_number = rand(10)
+            ota_string += random_number.to_str
+        end
+        self.update(ota_code: ota_string)
+    end
+
 end
