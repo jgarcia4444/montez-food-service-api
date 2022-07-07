00aa981c9eecd6ac576a39f3269118f4efb09dce
Jake Garcia <bballwiz4@gmail.com>
2022-07-07 14:53:15 -0700

    Created logic in the forgot_password action in the users_controller. This logic checks that information has been sent properly checks for a current user with the passed along information. Then creates a new ota code. Once code is created then sends an email to the users associated email.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,6 +37,64 @@ class UsersController < ApplicationController
     def show
     end
 
+    def forgot_password
+        if params[:user_info]
+            user_info = params[:user_info]
+            if user_info[:email]
+                email = user_info[:email]
+                user_to_send_code = User.find_by(email: email)
+                if user_to_send_code
+                    user_to_send_code.create_ota_code
+                    puts "User Validity! #{user_to_send_code.valid?}"
+                    if user_to_send_code.valid?
+                        begin
+                            UserNotifierMailer.send_code(user_to_send_code)
+                            render :json => {
+                                success: true,
+                            }
+                        rescue StandardError => e
+                            render :json => {
+                                success: false,
+                                error: {
+                                    message: "There was an error sending the email",
+                                    specificError: e.inspect
+                                }
+                            }
+                        end
+                    else
+                        render :json => {
+                            success: false,
+                            error: {
+                                message: "There was an error creating an authentication code."
+                            }
+                        }
+                    end
+                else 
+                    render :json => {
+                        success: false,
+                        error: {
+                            message: "No user was found with the given email."
+                        }
+                    }
+                end
+            else
+                render :json => {
+                    success: false,
+                    error: {
+                        message: "Users email is needed to reset the password."
+                    }
+                }
+            end
+        else 
+            render :json => {
+                success: false,
+                error: {
+                    message: "User information was not sent properly."
+                }
+            }
+        end
+    end
+
     def user_params
         params.require(:user_info).permit(:email, :password, :company_name)
     end
