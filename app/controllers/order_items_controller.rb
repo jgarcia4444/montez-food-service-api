fde76e29f1d6808861c1297d1d5e401480f1026f
Jake Garcia <bballwiz4@gmail.com>
2022-07-26 18:57:48 -0700

    Added in logic to fetch suggestions based off the users input in the item name. Also added a private function that will format the order item for the frontend.

---
+++ app/controllers/order_items_controller.rb
@@ -0,0 +1,47 @@
+class OrderItemsController < ApplicationController
+
+    def fetch_suggestions
+        puts "FETCH SUGGESTIONS ACTION TRIGGERED"
+        if params[:item_query]
+            item_query = params[:item_query]
+            suggestions = []
+            OrderItem.all.each do |order_item|
+                if suggestions.count > 9
+                    break
+                else
+                    downcased_description = order_item.description.downcase
+                    if downcased_description.include?(item_query)
+                        suggestions.push(format_order_item(order_item))
+                    end
+                end
+            end
+            if suggestions.count > 0
+                render :json => {
+                    success: true,
+                    suggestions: suggestions,
+                }
+            else
+                render :json => {
+                    success: false,
+                    error: {
+                        message: "Unable to find suggestions."
+                    }
+                }
+            end
+        end
+    end
+
+    private
+    def format_order_item(order_item)
+        {
+            description: order_item.description,
+            upc: order_item.upc,
+            item: order_item.item,
+            price: order_item.price,
+            costPerUnit: order_item.cost_per_unit,
+            caseCost: order_item.case_cost,
+            fiveCaseCost: order_item.five_case_cost,
+        }
+    end
+
+end
