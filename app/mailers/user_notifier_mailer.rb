bbf727578652a42bbb922dd8bdec921a0e7676ef
Jake Garcia <bballwiz4@gmail.com>
2022-08-21 09:53:05 -0700

    Added a method to the user notification mailer send_order_confirmation that sends an email to the users email that just placed an order. It also adds the order info as an instance method to pass to the html erb file. Created the mailer view that dispalys the order in a table by iterating through the items passed through the order info instance variable.

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,7 +1,15 @@
 class UserNotifierMailer < ApplicationMailer
+    
     default from: "montez.food.service.auth@gmail.com"
+
     def send_code(user)
         @user = user
         mail to: @user.email, subject: "Reset Password Code"
     end
+
+    def send_order_confirmation(order_info, user_email)
+        @order_info = order_info
+        mail to: user_email, subject: "Order Confirmation"
+    end
+
 end
