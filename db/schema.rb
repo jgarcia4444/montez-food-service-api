2a2dc74db1506c012c3ab28044377d18332ba0e1
Jake Garcia <bballwiz4@gmail.com>
2022-11-29 22:10:13 -0800

    Executed the created migrations to add more information to a users record as well as to create the addresses table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_08_014939) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_30_060719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "number", default: ""
+    t.string "street_name", default: ""
+    t.string "city", default: ""
+    t.string "state", default: ""
+    t.string "zip_code", default: ""
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.string "upc"
@@ -66,6 +77,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_08_014939) do
     t.string "ota_code"
     t.boolean "verified", default: false
     t.boolean "is_ordering", default: false
+    t.string "first_name", default: ""
+    t.string "last_name", default: ""
+    t.string "phone_number", default: ""
+    t.integer "address_id"
   end
 
 end
