7884cfd4275843f071de62ad5b3c513afec46ee0
Jake Garcia <bballwiz4@gmail.com>
2022-12-16 09:30:53 +0530

    Changed the addresses table so that the number and street name properties are now one under a street property.

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_30_060719) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_16_035841) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
-    t.string "number", default: ""
-    t.string "street_name", default: ""
     t.string "city", default: ""
     t.string "state", default: ""
     t.string "zip_code", default: ""
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "street", default: ""
   end
 
   create_table "order_items", force: :cascade do |t|
