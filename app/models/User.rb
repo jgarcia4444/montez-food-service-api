e51850f6cddf348389bbd6c9fa27cc1e2942821b
Jake Garcia <bballwiz4@gmail.com>
2022-12-26 09:50:12 -0800

    When a user logs in and they have past orders. the address id is now passed along with all the other address information.

--- app/models/User.rb
+++ app/models/User.rb
@@ -84,6 +84,7 @@ class User < ApplicationRecord
             specific_ordered_items = ordered_items.select {|ordered_item| ordered_item.user_order_id == user_order.id}
             past_order_address = Address.find_by(id: user_order.address_id)
             order_address = {
+                addressId: user_order.address_id,
                 street: past_order_address.street,
                 city: past_order_address.city,
                 state: past_order_address.state,
