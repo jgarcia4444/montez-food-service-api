3bc11542270a8524de60165c4cfc366bf0bee4af
Jake Garcia <bballwiz4@gmail.com>
2023-01-17 14:52:57 -0800

    Moved the oauth params into the quickbooks controller

--- app/controllers/quickbooks_controller.rb
+++ app/controllers/quickbooks_controller.rb
@@ -1,5 +1,12 @@
 
 class QuickbooksController < ApplicationController
+
+    oauth_params = {
+        site: "https://appcenter.intuit.com/connect/oauth2",
+        authorize_url: "https://appcenter.intuit.com/connect/oauth2",
+        token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+    }
+    oauth2_client = OAuth2::Client.new(ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET'], oauth_params)
     
     def authenticate
         redirect_uri = "https://montez-food-service-web.vercel.app/users/admin"
