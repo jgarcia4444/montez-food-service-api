6381be19584a6868674d7e96f03b12a53cf60438
Jake Garcia <bballwiz4@gmail.com>
2023-01-17 16:32:52 -0800

    Changed the redirect url

--- app/controllers/quickbooks_controller.rb
+++ app/controllers/quickbooks_controller.rb
@@ -15,11 +15,18 @@ class QuickbooksController < ApplicationController
     end
 
     def oauth_callback
+        oauth_params = {
+            site: "https://appcenter.intuit.com/connect/oauth2",
+            authorize_url: "https://appcenter.intuit.com/connect/oauth2",
+            token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+        }
+        oauth2_client = OAuth2::Client.new(ENV['OAUTH2_CLIENT_ID'], ENV['OAUTH2_CLIENT_SECRET'], oauth_params)
         puts "Params from the oauth_callback action---------"
         puts params
-        redirect_uri = "https://montez-food-service-api.herokuapp.com/oauth/callback"
+        redirect_uri = "https://montez-food-service-web.vercel.app/users/admin"
         if resp = oauth2_client.auth_code.get_token(params[:code], redirect_uri: redirect_uri)
-            
+            puts "Resp from getting a token"
+            puts resp
         else
             render :json => {
                 success: false,
