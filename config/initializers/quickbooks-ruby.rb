a5b7032251989a029c875bbf04a1bac3be8bd227
Jake Garcia <bballwiz4@gmail.com>
2023-01-18 14:09:52 -0800

    Commented out the initializer code.

--- config/initializers/quickbooks-ruby.rb
+++ config/initializers/quickbooks-ruby.rb
@@ -1,7 +1,7 @@
 
-oauth_params = {
-    site: "https://appcenter.intuit.com/connect/oauth2",
-    authorize_url: "https://appcenter.intuit.com/connect/oauth2",
-    token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
-}
-OAUTH2_CLIENT = OAuth2::Client.new(ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET'], oauth_params)
+# oauth_params = {
+#     site: "https://appcenter.intuit.com/connect/oauth2",
+#     authorize_url: "https://appcenter.intuit.com/connect/oauth2",
+#     token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+# }
+# OAUTH2_CLIENT = OAuth2::Client.new(ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET'], oauth_params)
