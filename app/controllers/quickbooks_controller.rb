de2a3004039baecf567e989fe71797354bf62ffc
Jake Garcia <bballwiz4@gmail.com>
2023-01-18 13:35:05 -0800

    Changed the oauth2 variable to an instance variable

--- app/controllers/quickbooks_controller.rb
+++ app/controllers/quickbooks_controller.rb
@@ -8,6 +8,8 @@ class QuickbooksController < ApplicationController
         #     token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
         # }
         # oauth2_client = OAuth2::Client.new(ENV['OAUTH2_CLIENT_ID'], ENV['OAUTH2_CLIENT_SECRET'], oauth_params)
+        puts "PARAMS ----- FROM OAUTH CALLBACK"
+        puts params
         redirect_uri = "https://montez-food-service-web.vercel.app/users/admin"
         if resp = OAUTH2_CLIENT.auth_code.get_token(params[:code], redirect_uri: redirect_uri)
             puts "RESP------"
