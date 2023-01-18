b4f4fb937bb4b91a819c3aee93ecf18c4b28db0e
Jake Garcia <bballwiz4@gmail.com>
2023-01-18 13:37:48 -0800

    Changed the oauth2 variable to an instance variable

--- app/controllers/quickbooks_controller.rb
+++ app/controllers/quickbooks_controller.rb
@@ -2,16 +2,16 @@
 class QuickbooksController < ApplicationController
 
     def oauth_callback
-        # oauth_params = {
-        #     site: "https://appcenter.intuit.com/connect/oauth2",
-        #     authorize_url: "https://appcenter.intuit.com/connect/oauth2",
-        #     token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
-        # }
-        # oauth2_client = OAuth2::Client.new(ENV['OAUTH2_CLIENT_ID'], ENV['OAUTH2_CLIENT_SECRET'], oauth_params)
+        oauth_params = {
+            site: "https://appcenter.intuit.com/connect/oauth2",
+            authorize_url: "https://appcenter.intuit.com/connect/oauth2",
+            token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
+        }
+        oauth2_client = OAuth2::Client.new(ENV['OAUTH2_CLIENT_ID'], ENV['OAUTH2_CLIENT_SECRET'], oauth_params)
         puts "PARAMS ----- FROM OAUTH CALLBACK"
         puts params
         redirect_uri = "https://montez-food-service-web.vercel.app/users/admin"
-        if resp = OAUTH2_CLIENT.auth_code.get_token(params[:code], redirect_uri: redirect_uri)
+        if resp = oauth2_client.auth_code.get_token(params[:code], redirect_uri: redirect_uri)
             puts "RESP------"
             puts resp
             render :json => {
