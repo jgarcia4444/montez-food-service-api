adeffc6deba1555fea403b48db6ef1f07b8828a3
Jake Garcia <bballwiz4@gmail.com>
2023-01-18 14:20:48 -0800

    Fixed the environment variables name

--- config/initializers/quickbooks-ruby.rb
+++ config/initializers/quickbooks-ruby.rb
@@ -4,4 +4,4 @@ oauth_params = {
     authorize_url: "https://appcenter.intuit.com/connect/oauth2",
     token_url: "https://oauth.platform.intuit.com/oauth2/v1/tokens/bearer"
 }
-OAUTH2_CLIENT = OAuth2::Client.new(ENV['OAUTH_CLIENT_ID'], ENV['OAUTH_CLIENT_SECRET'], oauth_params)
+OAUTH2_CLIENT = OAuth2::Client.new(ENV['OAUTH2_CLIENT_ID'], ENV['OAUTH2_CLIENT_SECRET'], oauth_params)
