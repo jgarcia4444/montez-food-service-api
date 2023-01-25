5931ee03bc48076d2ce5d19995329055602d72d7
Jake Garcia <bballwiz4@gmail.com>
2023-01-24 19:31:05 -0800

    Added a migration that will add a column to the ordered items table.

--- db/migrate/20230125032830_add_case_bought_to_ordered_items.rb
+++ db/migrate/20230125032830_add_case_bought_to_ordered_items.rb
@@ -1,5 +1,5 @@
 class AddCaseBoughtToOrderedItems < ActiveRecord::Migration[7.0]
   def change
-    add_column :ordered_items :case_bought, :boolean, default: false
+    add_column :ordered_items, :case_bought, :boolean, default: false
   end
 end
