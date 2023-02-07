3ec71caf29a4c7da2351d1aacb725b87669894d1
Jake Garcia <bballwiz4@gmail.com>
2023-02-06 17:31:04 -0800

    Changed a call to serviced_customer.id to user.quickbooks_id.

--- app/models/Admin.rb
+++ app/models/Admin.rb
@@ -70,7 +70,7 @@
         invoice = Quickbooks::Model::Invoice.new
         invoice.txn_date = DateTime.current
         invoice.billing_email_address = user.email
-        invoice.customer_id = serviced_customer.id
+        invoice.customer_id = user.quickbooks_id
         items = invoice_info[:items]
         invoice_with_line_items = Admin.add_line_items(items, invoice)
 
