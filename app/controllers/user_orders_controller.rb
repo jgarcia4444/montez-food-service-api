9a7547cb1ef0b3f75b80f6ca6ae8ca13dfdd3409
Jake Garcia <bballwiz4@gmail.com>
2023-02-20 19:29:57 -0800

    Moved the email sent to the admin in the same begin rescue block as sending the email to confirm the order to the user to avoid a double render.

--- app/controllers/user_orders_controller.rb
+++ app/controllers/user_orders_controller.rb
@@ -49,23 +49,12 @@ class UserOrdersController < ApplicationController
                                         orderDate: user_order.created_at,
                                         orderAddress: address,
                                     }
-                                    begin 
-                                        UserNotifierMailer.send_order_confirmation(past_order_info, user_email).deliver_now
-                                        
-                                    rescue StandardError => e
-                                        render :json => {
-                                            success: false,
-                                            error: {
-                                                message: "There was an error sending the order confirmation email",
-                                                specificError: e.inspect
-                                            }
-                                        }
-                                    end
                                     montez_new_order_info = {
                                         total_price: user_order.total_price,
                                         company_name: user.company_name,
                                     }
-                                    begin
+                                    begin 
+                                        UserNotifierMailer.send_order_confirmation(past_order_info, user_email).deliver_now
                                         UserNotifierMailer.send_order_alert(montez_new_order_info).deliver_now
                                         render :json => {
                                             success: true,
@@ -75,7 +64,7 @@ class UserOrdersController < ApplicationController
                                         render :json => {
                                             success: false,
                                             error: {
-                                                message: "There was an error sending the new order received email.",
+                                                message: "There was an error sending the order confirmation email",
                                                 specificError: e.inspect
                                             }
                                         }
