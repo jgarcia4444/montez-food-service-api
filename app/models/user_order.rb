4e534083296741891bb974e9b91d2e1f9ec71be7
Jake Garcia <bballwiz4@gmail.com>
2023-02-24 15:12:38 -0800

    Fixed a syntax error in the user_order model.

--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -94,7 +94,7 @@ class UserOrder < ApplicationRecord
                 access_token = OAuth2::AccessToken.new(OAUTH2_CLIENT, service_info[:access_token], refresh_token: service_info[:refresh_token])
                 invoice_service = Quickbooks::Service::Invoice.new
                 invoice_service.access_token = access_token
-                invoice_service.company_id = service[:realm_id]
+                invoice_service.company_id = service_info[:realm_id]
                 invoices = invoice_service.query("Select * From Invoice Where CustomerRef = '#{customer_ref}'")
                 puts invoices
                 address = Address.find_by(id: self.address_id)
